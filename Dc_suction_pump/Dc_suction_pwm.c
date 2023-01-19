4eef505c8792ef4c524d17ca64d1bee6783a3e3e
Tarun_Gautam <tarungautam@agvahealthtech.com>
2023-01-19 10:33:56 +0530

    minor updates in the code

--- Dc_suction_pump/Dc_suction_pwm.c
+++ Dc_suction_pump/Dc_suction_pwm.c
@@ -2,23 +2,23 @@
 
 void TCA0_PWM_init(void)
 {
-	PORTA.DIR |= (1 << 0);		// pump pin
-	PORTA.DIR |= (1 << 1);	    // for proportional valve
+	PORTA.DIR |= (1 << 0);			// pump pin
+	PORTA.DIR |= (1 << 1);			// for proportional valve
 	PORTA.PIN0CTRL |= (1 << 3);
-//	PORTA.PIN1CTRL |= (1 << 3);
+	//	PORTA.PIN1CTRL |= (1 << 3);
 	//PORTA.OUTSET |= (1 << 0);
-	//TCA0.SINGLE.PER = 120;     // calcu lation for 40us
-	TCA0.SINGLE.PER = 3000;      // calculation for 1ms
-	TCA0.SINGLE.CMP0 = 0;	//pump pwm
-// 	TCA0.SINGLE.CMP1 = 0;	// proportional valve pwm
+	//TCA0.SINGLE.PER = 120;		// calcu lation for 40us
+	TCA0.SINGLE.PER = 3000;			// calculation for 1ms
+	TCA0.SINGLE.CMP0 = 0;			//pump pwm
+	// 	TCA0.SINGLE.CMP1 = 0;		// proportional valve pwm
 	
 	TCA0.SINGLE.CNT = 0;
-	TCA0.SINGLE.CTRLB |= (1 << 4); // compare 0 enable
-	//TCA0.SINGLE.CTRLB |= (1 << 5); // compare 1 enable
-	TCA0.SINGLE.CTRLB |= (0x3 << 0); // Single slope PWM	
-	TCA0.SINGLE.CTRLA |= (1<<0); //fclk_per 1 and  Enable pwm
-	//TCA0.SINGLE.CTRLA |= (0x3 << 1); //fclk_per 8 and  Enable pwm
-	TCA0.SINGLE.CTRLA |= (0x1 << 1); //fclk_per 4 and  Enable pwm
+	TCA0.SINGLE.CTRLB |= (1 << 4);				// compare 0 enable
+	//TCA0.SINGLE.CTRLB |= (1 << 5);			// compare 1 enable
+	TCA0.SINGLE.CTRLB |= (0x3 << 0);			// Single slope PWM	
+	TCA0.SINGLE.CTRLA |= (1<<0);				//fclk_per 1 and  Enable pwm
+	//TCA0.SINGLE.CTRLA |= (0x3 << 1);			//fclk_per 8 and  Enable pwm
+	TCA0.SINGLE.CTRLA |= (0x1 << 1);			//fclk_per 4 and  Enable pwm
 }
 /*
 void TCA0_PWM_init(void)
@@ -26,7 +26,7 @@ void TCA0_PWM_init(void)
 	
 	PORTA.DIR |= (1 << 0);
 	PORTA.PIN0CTRL |= (1 << 3);
-	TCA0.SINGLE.PER = 4000;						 // calculation for 1ms
+	TCA0.SINGLE.PER = 4000;						// calculation for 1ms
 	TCA0.SINGLE.CNT = 0;
 	TCA0.SINGLE.CTRLA |= (0x0 << 1);			//Ftca = fclk_per
 	TCA0.SINGLE.CTRLB |= (1 << 4);				// compare 0 enable
